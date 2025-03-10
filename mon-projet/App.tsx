3a01faff9ec226c4d5c53ec00410fe115e561ea4
vikingprank <vikingprank@outlook.com>
2025-03-10 18:57:33 +0100

    Moved button logic to App.tsx

--- mon-projet/App.tsx
+++ mon-projet/App.tsx
@@ -4,7 +4,7 @@ import { determineNumberOfLettersToHide } from "./src/game/determineNumberOfLett
 import { chooseLettersToHide } from "./src/game/chooseLettersToHide";
 import { randomlyChooseExtraLetters } from "./src/game/randomlyChooseExtraLetters";
 import { filterOutHiddenLetters } from "./src/game/filterOutHiddenLetters";
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import { shuffleArray } from "./src/game/shuffleLetters";
 import { LetterButton } from "./src/components/LetterButton";
 
@@ -14,20 +14,50 @@ const RowView: React.FC<{
   children: ReactNode;
 }> = ({ children }) => <View style={{ flexDirection: "row" }}>{children}</View>;
 
+export enum TriedState {
+  NOT_TRIED = "not_tried",
+  TRIED_AND_FALSE = "tried_and_false",
+  TRIED_AND_TRUE = "tried_and_true",
+}
+
+type PropositionsState = [string, TriedState];
+
+const numberOfLettersToHide = determineNumberOfLettersToHide(WORD_TO_GUESS);
+const lettersToHide = chooseLettersToHide(WORD_TO_GUESS, numberOfLettersToHide);
+const otherPropositions = randomlyChooseExtraLetters(lettersToHide);
+const wordWithHiddenLetters = filterOutHiddenLetters(
+  WORD_TO_GUESS,
+  lettersToHide
+);
+const allPossibleAnswers = shuffleArray(
+  lettersToHide.concat(otherPropositions)
+);
+
 export default function App() {
-  const numberOfLettersToHide = determineNumberOfLettersToHide(WORD_TO_GUESS);
-  const lettersToHide = chooseLettersToHide(
-    WORD_TO_GUESS,
-    numberOfLettersToHide
-  );
-  const otherPropositions = randomlyChooseExtraLetters(lettersToHide);
-  const wordWithHiddenLetters = filterOutHiddenLetters(
-    WORD_TO_GUESS,
-    lettersToHide
-  );
-  const allPossibleAnswers = shuffleArray(
-    lettersToHide.concat(otherPropositions)
-  );
+  const [displayedWord, setDisplayedWord] = useState(wordWithHiddenLetters);
+
+  const initialPropositionsState: PropositionsState[] = {
+    ...allPossibleAnswers.map((letter) => {
+      return [letter, TriedState.NOT_TRIED];
+    }),
+  };
+
+  const [propositionsState, setPropositionsState] = useState<
+    PropositionsState[]
+  >(initialPropositionsState);
+
+  console.log(JSON.stringify(initialPropositionsState, null, 2));
+
+  const handlePressButton = (index: number) => {
+    setPropositionsState((prevState) => {
+      const newTries = prevState; // Shallow Copy
+      const currentEntry = newTries[index];
+      if (currentEntry) {
+        prevState[index] = [currentEntry[0], TriedState.TRIED_AND_TRUE];
+      }
+      return { ...newTries };
+    });
+  };
 
   return (
     <View style={styles.container}>
@@ -41,11 +71,17 @@ export default function App() {
           );
         })}
       </RowView>
-
       <RowView>
         <Text numberOfLines={2}>
           {allPossibleAnswers.map((letter, index) => {
-            return <LetterButton key={index} letter={letter} />;
+            return (
+              <LetterButton
+                key={index}
+                letter={letter}
+                letterState={propositionsState[index][1] as TriedState}
+                onCallBackPress={() => handlePressButton(index)}
+              />
+            );
           })}
         </Text>
       </RowView>
--- mon-projet/src/components/LetterButton.tsx
+++ mon-projet/src/components/LetterButton.tsx
@@ -1,20 +1,31 @@
-import { TouchableOpacity, Text } from "react-native";
-import { FONT_SIZE } from "../../App";
+import { TouchableOpacity, Text, GestureResponderEvent } from "react-native";
+import { FONT_SIZE, TriedState } from "../../App";
+import { useState } from "react";
 
 export const LetterButton: React.FC<{
   letter: string;
-}> = ({ letter }) => (
-  <TouchableOpacity onPress={() => console.log({ letter })}>
-    <Text
-      style={{
-        borderWidth: 1,
-        borderRadius: 2,
-        fontSize: FONT_SIZE,
-        padding: 3,
-        margin: 3,
-      }}
-    >
-      {letter}
-    </Text>
-  </TouchableOpacity>
-);
+  letterState: TriedState;
+  onCallBackPress: () => void;
+}> = ({ letter, letterState, onCallBackPress }) => {
+  return (
+    <TouchableOpacity onPress={onCallBackPress}>
+      <Text
+        style={{
+          borderWidth: 1,
+          borderRadius: 2,
+          fontSize: FONT_SIZE,
+          padding: 3,
+          margin: 3,
+          backgroundColor:
+            letterState === TriedState.NOT_TRIED
+              ? "transparent"
+              : letterState === TriedState.TRIED_AND_FALSE
+              ? "red"
+              : "green",
+        }}
+      >
+        {letter}
+      </Text>
+    </TouchableOpacity>
+  );
+};
