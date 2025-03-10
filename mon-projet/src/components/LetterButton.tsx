881fdf9aed856eb1f7906415fb09a0cb23d779d3
vikingprank <vikingprank@outlook.com>
2025-03-10 23:12:25 +0100

    Disable button when already pressed

--- mon-projet/src/components/LetterButton.tsx
+++ mon-projet/src/components/LetterButton.tsx
@@ -7,19 +7,22 @@ export const LetterButton: React.FC<{
   onCallBackPress: () => void;
 }> = ({ letter, letterState, onCallBackPress }) => {
   return (
-    <TouchableOpacity onPress={onCallBackPress}>
+    <TouchableOpacity
+      onPress={onCallBackPress}
+      disabled={letterState !== TriedState.NOT_TRIED ? true : false}
+    >
       <Text
         style={{
-          textAlign: 'center', 
+          textAlign: "center",
           width: 40,
           height: 40,
           borderWidth: 1,
           borderRadius: 10,
           fontSize: 20,
-          fontWeight: '900',
+          fontWeight: "900",
           padding: 5,
           margin: 5,
-          borderBottomColor: 'darkgrey',
+          borderBottomColor: "darkgrey",
           borderBottomWidth: 3,
           backgroundColor:
             letterState === TriedState.NOT_TRIED
